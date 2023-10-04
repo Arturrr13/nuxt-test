f9bb5888a68fdfb77d5b08b2a236e215a5310e91
Arturrr13 <camyrauu911@gmail.com>
2023-10-04 13:48:05 +0300

    Initial dist subtree commit

---
+++ dist/_nuxt/login.b8d5517c.js
@@ -0,0 +1,1 @@
+import{_ as o,c as n,C as r,o as t}from"./entry.3ec37315.js";const c={};function s(e,a){return t(),n("div",null,[r(e.$slots,"default")])}const _=o(c,[["render",s]]);export{_ as default};
